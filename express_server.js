e4041299abaa02ad60872cf5e4ff1eec68da819c
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-17 18:50:59 +0000

    added additional route

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
