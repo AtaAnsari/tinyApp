77ccd14c53a706988619aaeff8778e17e6afaed3
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-17 20:46:54 +0000

    filled out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -19,6 +19,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
