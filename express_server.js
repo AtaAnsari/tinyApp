2f02f0572dec3d15d76d6020e1004f3e2dc46ba6
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-17 23:36:11 +0000

    added webform and routed it to the server, created a function that generates a random alphanumeric string of 6 chars

--- express_server.js
+++ express_server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -30,7 +35,28 @@ app.listen(PORT, () => {
   
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const randomString = function(){
+  const p = "abcdefghijklmnopqrstuvwxyz0123456789"
+  const startingArray = p.split("")
+  let finalArray = []
+  for(i = 0; i < 6; i++) {
+    finalArray.push(startingArray[Math.floor(Math.random()*36)])
+  }
+  return finalArray.join("")
+  
+}
+
