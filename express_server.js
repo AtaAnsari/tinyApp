a6fcf6bb6d1cfe8e784e5bc075674ec98fedcc3d
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-18 00:42:16 +0000

    final commit

--- express_server.js
+++ express_server.js
@@ -39,24 +39,38 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+// app.get("/urls/:shortURL", (req, res) => {
+//   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+//   res.render("urls_show", templateVars);
+// });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const randomString = function(){
+    const p = "abcdefghijklmnopqrstuvwxyz0123456789"
+    const startingArray = p.split("")
+    let finalArray = []
+    for(i = 0; i < 6; i++) {
+      finalArray.push(startingArray[Math.floor(Math.random()*36)])
+    }
+    return finalArray.join("")
+    
+  }
+  const newShortURL = randomString();
+  const newLongURL = req.body["longURL"]
+  urlDatabase[newShortURL] = newLongURL
+
+  console.log(urlDatabase);  // Log the POST request body to the console
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let templateVars = { shortURL: newShortURL, longURL: newLongURL};
+  res.render("urls_show", templateVars);
 });
 
-const randomString = function(){
-  const p = "abcdefghijklmnopqrstuvwxyz0123456789"
-  const startingArray = p.split("")
-  let finalArray = []
-  for(i = 0; i < 6; i++) {
-    finalArray.push(startingArray[Math.floor(Math.random()*36)])
-  }
-  return finalArray.join("")
-  
-}
+
+
+
 
