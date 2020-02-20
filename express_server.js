75fee72bad442c7ec1affda5db2dfdfd499557c1
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-20 01:29:15 +0000

    fulfilled all feature requirements

--- express_server.js
+++ express_server.js
@@ -32,6 +32,33 @@ const emailExists = function(email){
   return false
 };
 
+// passwordFinder helper function
+const passwordExists = function(password){
+  for(Id in users){
+    if(users[Id]["password"] === password) {
+      return true
+    }
+  }
+};
+
+// ID catcher helper function 
+const idCatcher = function(password){
+  for(Id in users){
+    if(users[Id]["password"] === password) {
+      return Id
+    }
+  }
+  return false
+};
+
+// Confirm that the cx username and pw match
+const identityConfirm = function (email, password) {
+  if(users[idCatcher(password)]["email"] === email){
+    console.log("email matches")
+    return true
+  }
+}
+
 // Defining Database
 
 const urlDatabase = {
@@ -56,6 +83,11 @@ const users = {
     id: "user2RandomID", 
     email: "user2@example.com", 
     password: "dishwasher-funk"
+  },
+  "asds":{
+    id: "asds",
+    email: "l@gmail.com",
+    password: "l"
   }
 }
 
@@ -64,7 +96,6 @@ const users = {
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase,
-    // username: req.cookies["username"],
     user: users[req.cookies["user_id"]]
   };
   res.render("urls_index", templateVars);
@@ -74,7 +105,6 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    // username: req.cookies["username"] 
     user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
@@ -82,19 +112,25 @@ app.get("/urls/new", (req, res) => {
 
 // Establishing a route to the registration page
 app.get("/register", (req, res) => {
-  let templateVars = {
-    // username: req.cookies["username"] 
+  let templateVars = { 
     user: users[req.cookies["user_id"]]
   };
   res.render("urls_register", templateVars);
 });
 
+// Establishing a route to /login endpoint
+
+app.get("/login", (req, res) => {
+  let templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("urls_login", templateVars);
+});
 
 // setting up route to the page that the user will be redirected to when they create a new shortURL
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],
-  // username: req.cookies["username"] 
   user: users[req.cookies["user_id"]]
 };
   res.render("urls_show", templateVars);
@@ -137,17 +173,33 @@ app.post("/urls/:url/edit", (req, res) => {
   res.redirect("/urls/" + req.params.url);
 });
 
-// setting username cookie once username is submitted
+// setting up route for login link on header
 
+app.get("/loginButton", (req, res) => {
+  res.redirect("/login")
+});
+
+// handling post request from login page
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body["username"] )
-  res.redirect("/urls")
+  const userId = randomString();
+  const email = req.body["email"]
+  const password = req.body["password"]
+  if(!emailExists(email)){
+    res.status(403).send("403 ERROR")
+  } else {
+    if(passwordExists(password) === true && identityConfirm(email, password) === true){
+      res.cookie("user_id", idCatcher(password) )
+      res.redirect("/urls");
+    } else {
+    res.status(403).send("403 ERROR")
+  }
+}
 });
 
 // clearing cookie once the logout button is clicked. 
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
+  res.clearCookie("user_id")
   res.redirect("/urls")
 });
 
